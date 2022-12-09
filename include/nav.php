3bead9e25846afe0877e459293e8cabb9c3ae5ce
Juan Senen <95066970+JSenen@users.noreply.github.com>
2022-12-09 18:41:35 +0100

    Añadido enlace a navegador

--- include/nav.php
+++ include/nav.php
@@ -3,7 +3,8 @@ session_start();
 
 ?>
 <nav class="navegacion">
-        <a href="#" class="navegacion--activo">Inicio</a>
+        <a href="#" >Inicio</a>
+        <a href="#" >Enviar Email</a>
         <a href="logout.php">Login Out <?php echo $_SESSION['name'] ?></a>
 </nav>
 
