59d7c8802c78eae97f15aec0a2b15beb699b35a7
Juan Senen <95066970+JSenen@users.noreply.github.com>
2022-12-09 15:51:27 +0100

    Finalizado login de inicio y log out

--- include/session.php
+++ include/session.php
@@ -1,13 +1,26 @@
 <?php
 require_once('./domain/Customer.php');
 require_once('./domain/Account.php');
-session_start();
 
 //Creamos un cliente y cuentas de prueba
 $accounts = [new Account('ES3045678912','Debito', 1267.5),
              new Account('ES0937568235','Inversion',3487.67)];
-$costumer1 = new Customer('Juanito','Garcia','Lopez','juan@email.com',"1234",$accounts);
+$costumer1 = new Customer('User','userfirst','userlast','juan@email.com',"1111",$accounts);
+?>
+<form method="post" action="" name="signin-form">
+    <div class="form-element">
+        <label>Username</label>
+        <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
+    </div>
+    <div class="form-element">
+        <label>Password</label>
+        <input type="password" name="password" required />
+    </div>
+    <button type="submit" name="login" value="login">Log In</button>
+</form>
+<?php
 
+//Validamos el login del formulario
 if (isset($_POST['login'])) {
 
     $username = $_POST['username'];
@@ -15,21 +28,18 @@ if (isset($_POST['login'])) {
 
 
     if ($username == $costumer1->name && $password == $costumer1->password) {
-        echo '<p class="error">LOGIN CORRECTO</p>';
-
+        echo '<div class="message_login"><div><p class="success"><a href="account.php">LOGIN CORRECTO pulse aqui</a></p></div></div></p>';
+        session_start(); //Inicio de sesion
+        $_SESSION['name'] = $costumer1->name; //Asociamos sesion al nombre de usuario
+        setcookie('BANK2_COKKIE','',86400);
+    }else{
+        session_write_close(); //Borramos sesiones anteriores
+        echo '<div class="message_login"><div><p class="error">LOGIN INCORRECTO</p></div></div></p>';
     }
 }
-?>
-<form method="post" action="" name="signin-form">
-        <div class="form-element">
-            <label>Username</label>
-            <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
-        </div>
-        <div class="form-element">
-            <label>Password</label>
-            <input type="password" name="password" required />
-        </div>
-        <button type="submit" name="login" value="login">Log In</button>
-    </form>
+
+
 
 ?>
+
+
